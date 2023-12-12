52957d01a4cacad2bdc4f66ef6137886a7a043f8
Lzynee <raina2zen90@gmail.com>
2023-12-12 17:01:03 +0900

    컨트롤러-서비스-list 뷰 연결

--- in_class/java_data/int_jsp/jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
+++ in_class/java_data/int_jsp/jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -1,11 +1,16 @@
 package org.zerock.jdbcex.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.zerock.jdbcex.dao.TodoDAO;
 import org.zerock.jdbcex.domain.TodoVO;
 import org.zerock.jdbcex.dto.TodoDTO;
 import org.zerock.jdbcex.util.MapperUtil;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log4j2
 public enum TodoService {
     INSTANCE;
 
@@ -30,4 +35,21 @@ public enum TodoService {
         dao.insert(todoVO);  // int를 반환하므로 이를 이용해서 예외처리도 가능
 
     }
+
+    // 컨트롤러와 TodoService를 연동한다.
+    // TodoDAO에서 가져온 TodoVO의 목록을 모두 TodoDTO로 변환해서 반환한다.
+    public List<TodoDTO> listAll() throws Exception {
+
+        List<TodoVO> voList = dao.selectAll();
+
+        log.info("voList =======================");
+        log.info(voList);
+
+        List<TodoDTO> dtoList = voList.stream()
+                .map(vo -> modelMapper.map(vo, TodoDTO.class))
+                .collect(Collectors.toList());
+
+        return dtoList;
+    }
+
 }
