fde3fb27cb8579f16de4a1edd9eb27abde3ded32
Lzynee <raina2zen90@gmail.com>
2023-12-13 10:43:49 +0900

    수정/삭제 기능 구현: 수정 기능 코드 추가

--- in_class/java_data/int_jsp/jdbcex/src/main/java/org/zerock/jdbcex/controller/TodoModifyController.java
+++ in_class/java_data/int_jsp/jdbcex/src/main/java/org/zerock/jdbcex/controller/TodoModifyController.java
@@ -6,10 +6,9 @@ import org.zerock.jdbcex.service.TodoService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @WebServlet(name = "todoModifyController", value = "/todo/modify")
@@ -44,7 +43,28 @@ public class TodoModifyController extends HttpServlet {
 
     // 수정 작업을 처리한다.
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String finishedStr = req.getParameter("finished");
+
+        TodoDTO todoDTO = TodoDTO.builder()
+                .tno(Long.parseLong(req.getParameter("tno")))
+                .title(req.getParameter("title"))
+                .dueDate(LocalDate.parse(req.getParameter("dueDate"),DATEFORMATTER))
+                .finished(finishedStr != null && finishedStr.equals("on"))
+                .build();
+
+        log.info("/todo/modify POST =======================");
+        log.info(todoDTO);
+
+        try {
+            todoService.modify(todoDTO);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        resp.sendRedirect("/todo/list");
     }
 }
