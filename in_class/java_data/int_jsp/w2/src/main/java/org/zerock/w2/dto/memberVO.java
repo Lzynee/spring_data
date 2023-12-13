38f5fecf23be1b4fb4599c6c055a635569478370
Lzynee <raina2zen90@gmail.com>
2023-12-13 16:05:08 +0900

    회원 데이터 처리를 위한 vo 파일 생성

---
+++ in_class/java_data/int_jsp/w2/src/main/java/org/zerock/w2/dto/memberVO.java
@@ -0,0 +1,15 @@
+package org.zerock.w2.dto;
+
+import lombok.*;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class memberVO {
+
+    private String mid;
+    private String mpw;
+    private String mname;
+}
