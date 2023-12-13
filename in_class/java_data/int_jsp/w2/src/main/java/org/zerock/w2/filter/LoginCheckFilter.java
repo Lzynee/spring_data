8cc4c4fccbec672eda6bb262b4a25720f7b1af69
Lzynee <raina2zen90@gmail.com>
2023-12-13 15:15:39 +0900

    로그인 여부를 체크하는 코드 추가

--- in_class/java_data/int_jsp/w2/src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
+++ in_class/java_data/int_jsp/w2/src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
@@ -4,6 +4,9 @@ import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = {"/todo/*"})  // /todo/...로 시작하는 모든 경로에 대해서 필터링을 시도한다.
@@ -15,6 +18,18 @@ public class LoginCheckFilter implements Filter {
             throws IOException, ServletException {
 
         log.info("Login check filter ...");
+
+        // 로그인 여부를 체크한다.
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse resp = (HttpServletResponse)response;
+
+        HttpSession session = req.getSession();
+
+        if (session.getAttribute("loginInfo") == null) {
+            resp.sendRedirect("/login");
+            return;
+        }
+        // 로그인 여부 체크 끝
 
         chain.doFilter(request, response);  // 다음 필터나 목적지로 갈 수 있도록 실행한다.
     }
