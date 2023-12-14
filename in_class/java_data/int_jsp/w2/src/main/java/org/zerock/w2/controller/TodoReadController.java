de323cb484ce7c2a96f5dfb0e70668315f405b17
Lzynee <raina2zen90@gmail.com>
2023-12-14 10:07:57 +0900

    목록에서 조회한 Todo 목록을 보관하기 위한 사용자 정의 쿠키 생성

--- in_class/java_data/int_jsp/w2/src/main/java/org/zerock/w2/controller/TodoReadController.java
+++ in_class/java_data/int_jsp/w2/src/main/java/org/zerock/w2/controller/TodoReadController.java
@@ -1,3 +1,9 @@
+/**
+ * Todo 목록에서 조회했던 Todo 번호들을 쿠키를 이용해서 보관한다.
+ * 본 컨트롤러의 기능:
+ * 1. 현재 요청(request)에 있는 모든 쿠키 중에 조회 목록 쿠키(viewTodos)를 찾아낸다.
+ * 2. 특정한 tno가 쿠키의 내용물이 있는지 확인한다.
+ * */
 package org.zerock.w2.controller;
 
 import lombok.extern.log4j.Log4j2;
@@ -27,11 +33,58 @@ public class TodoReadController extends HttpServlet {
             // 데이터 담기
             req.setAttribute("dto", todoDTO);
 
+            // 쿠키 찾기
+            // viewTodos 이름의 쿠키를 찾는다.
+            Cookie viewTodoCookie = findCookie(req.getCookies(), "viewTodos");
+            String todoListStr = viewTodoCookie.getValue();
+            boolean exist = false;
+
+            if (todoListStr != null && todoListStr.indexOf(tno + "-") >= 0) {
+                exist = true;
+            }
+
+            log.info("exist: " + exist);
+
+            // 쿠키를 변경할 때에는 다시 경로나 유효시간을 세팅해야 한다.
+            if (!exist) {
+                todoListStr += tno + "-";
+                viewTodoCookie.setValue(todoListStr);
+                viewTodoCookie.setMaxAge(60 * 60 * 24);  // 쿠키 유지 시간 (24시간)
+                viewTodoCookie.setPath("/");
+                resp.addCookie(viewTodoCookie);
+            }
+            // 쿠키 찾기 끝
             req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req,resp);
         }
         catch (Exception e) {
             log.error(e.getMessage());
             throw new ServletException("read error");
         }
+    }
+
+    private Cookie findCookie(Cookie[] cookies, String cookieName) {
+
+        Cookie targetCookie = null;
+
+        // 쿠키의 내용물 검사
+        if (cookies != null && cookies.length > 0) {
+
+            for (Cookie ck:cookies) {
+
+                if (ck.getName().equals(cookieName)) {
+                    targetCookie = ck;
+                    break;
+                }
+            }
+        }
+
+        // 조회한 적이 없는 번호라면 쿠키의 내용물을 갱신해서 브라우저로 보낸다.
+        if (targetCookie == null) {
+            targetCookie = new Cookie(cookieName, "");
+            targetCookie.setPath("/");
+            targetCookie.setMaxAge(60 * 60 * 24);  // 유효시간 : 60초 * 60 * 24 = 24시간
+        }
+
+        return targetCookie;
     }
 }
